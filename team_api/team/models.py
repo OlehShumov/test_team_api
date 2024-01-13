84b0f6f89197f2041395fdbad14a7a80d29db464
OlehShumov <shumovoleh@ukr.net>
2024-01-13 17:19:07 +0200

    change on_delete in models

--- team_api/team/models.py
+++ team_api/team/models.py
@@ -12,7 +12,7 @@ class Person(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
     email = models.EmailField()
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="people")
+    team = models.ForeignKey(Team, on_delete=models.DO_NOTHING, related_name="people")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.email})"
